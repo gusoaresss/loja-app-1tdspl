ae6051b5a90157d12280dd5a42d89570b1d9d068
Gustavo Soares <guusoaresss@icloud.com>
2023-10-17 09:50:50 -0300

    Criando rotas / rotas dinamicas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <h1>Home Page</h1>
+
+      <div>
+        <Link href="/produtos/tenis">Tênis</Link> | 
+        <Link href="/produtos/produto/tenis-preto">Tênis preto</Link>
+      </div>
     </>
   )
 }
---
+++ src/app/produtos/tenis/page.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+export default function Tenis() {
+    return (
+      <div>
+        <h1>Tênis</h1>
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste dolor ex ut non optio illum officia consequuntur atque modi iusto culpa, veritatis magnam quo tenetur natus? Praesentium facilis quidem impedit?
+            </p>
+            <Link href="/">Voltar...</Link>
+        </div>
+      </div>
+    )
+}
